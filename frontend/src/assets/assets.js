2cb52ec7dec128fcfc4cee94b8e3e598082142e3
BiswaJit0407 <biswajitpadhan2001@gmail.com>
2025-05-17 15:28:57 +0530

    Convert frontend submodule to normal folder

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,279 @@
+import men_b_24 from './men_b_24.png'
+import men_b_24_1 from './men_b_24_1.png'
+import men_b_24_2 from './men_b_24_2.png'
+import men_b_24_3 from './men_b_24_3.png'
+import rem_24 from './rem_24.png'
+import rem_24_1 from './rem_24_1.png'
+import rem_24_2 from './rem_24_2.png'
+import rem_24_3 from './rem_24_3.png'
+import Tm_24 from './Tm_24.png'
+import Tm_24_1 from './Tm_24_1.png'
+import Tm_24_2 from './Tm_24_2.png'
+import Tm_24_3 from './Tm_24_3.png'
+import Fm_24 from './Fm_24.png'
+import Fm_24_1 from './Fm_24_1.png'
+import Fm_24_2 from './Fm_24_2.png'
+import Fm_24_3 from './Fm_24_3.png'
+import AwayKit24 from './AwayKit24.png'
+import AwayKit24_1 from './AwayKit24_1.png'
+import AwayKit24_2 from './AwayKit24_2.png'
+import AwayKit24_3 from './AwayKit24_3.png'
+import HomeKit24 from './HomeKit24.png'
+import HomeKit24_1 from './HomeKit24_1.png'
+import HomeKit24_2 from './HomeKit24_2.png'
+import HomeKit24_3 from './HomeKit24_3.png'
+import Awayf_24 from './Awayf_24.png'
+import Awayf_24_1 from './Awayf_24_1.png'
+import Awayf_24_2 from './Awayf_24_2.png'
+import Awayf_24_3 from './Awayf_24_3.png'
+import Bfem from './Bfem.png'
+import Bfem_1 from './Bfem_1.png'
+import Bfem_2 from './Bfem_2.png'
+import Bfem_3 from './Bfem_3.png'
+import Homef_24 from './Homef_24.png'
+import Homef_24_1 from './Homef_24_1.png'
+import Homef_24_2 from './Homef_24_2.png'
+import Homef_24_3 from './Homef_24_3.png'
+import Retro_M from './Retro_M.png'
+import Retro_M_1 from './Retro_M_1.png'
+import Retro_M_2 from './Retro_M_2.png'
+import Retro_M_3 from './Retro_M_3.png'
+import BottomDP from './BottomDP.png'
+import BottomDP2 from './BottomDP2.png'
+import BottomDP3 from './BottomDP3.png'
+import BottomP from './BottomP.png'
+import BottomP1 from './BottomP1.png'
+import TShortWB from './TShortWB.png'
+import TShortWB1 from './TShortWB1.png'
+import TShortWB2 from './TShortWB2.png'
+import TShortWB3 from './TShortWB3.png'
+import UShortsWB from './UShortsWB.png'
+import UShortsWB1 from './UShortsWB1.png'
+import UShortsWB2 from './UShortsWB2.png'
+import UShortsWB3 from './UShortsWB3.png'
+
+
+import logo from './logo.png'
+import cart_icon from './cart_icon.png'
+import bin_icon from './bin_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import profile_icon from './profile_icon.png'
+import quality_icon from './quality_icon.png'
+import search_icon from './search_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import support_img from './support_img.png'
+import menu_icon from './menu_icon.png'
+import about_img from './about_img.png'
+import contact_img from './contact_img.png'
+import razorpay_logo from './razorpay_logo.png'
+import stripe_logo from './stripe_logo.png'
+import cross_icon from './cross_icon.png'
+
+
+
+export const assets = {
+    logo,
+    cart_icon,
+    dropdown_icon,
+    exchange_icon,
+    profile_icon,
+    quality_icon,
+    search_icon,
+    star_dull_icon,
+    star_icon,
+    bin_icon,
+    support_img,
+    menu_icon,
+    about_img,
+    contact_img,
+    razorpay_logo,
+    stripe_logo,
+    cross_icon
+}
+
+export const products = [
+    {
+        _id: "menretro",
+         name: "Men Retro White",
+         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+         price: 100,
+         image: [rem_24,rem_24_1,rem_24_2,rem_24_3],
+         category: "Men",
+         subCategory: "Topwear",
+         sizes: ["S", "M", "L"],
+         date: 1716634345448,
+         bestseller: true 
+     },
+     {
+        _id: "awayKit",
+         name: "Men Away Jersey",
+         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+         price: 100,
+         image: [AwayKit24,AwayKit24_1,AwayKit24_2,AwayKit24_3],
+         category: "Men",
+         subCategory: "Topwear",
+         sizes: ["S", "M", "L"],
+         date: 1716634345448,
+         bestseller: true 
+     },
+     {
+        _id: "HomeKit",
+         name: "Men Home Jersey",
+         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+         price: 100,
+         image: [HomeKit24,HomeKit24_1,HomeKit24_2,HomeKit24_3],
+         category: "Men",
+         subCategory: "Topwear",
+         sizes: ["S", "M", "L"],
+         date: 1716634345448,
+         bestseller: true 
+     },
+     {
+        _id: "retroMenBlack",
+         name: "Retro Men Black",
+         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+         price: 100,
+         image: [Retro_M,Retro_M_1,Retro_M_2,Retro_M_3],
+         category: "Men",
+         subCategory: "Topwear",
+         sizes: ["S", "M", "L"],
+         date: 1716634345448,
+         bestseller: true 
+     },
+     {
+        _id: "WomAway",
+        name: "Women Away Kit Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 100,
+        image: [Awayf_24,Awayf_24_1,Awayf_24_2,Awayf_24_3],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "WomBlack",
+        name: "Women Black Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 100,
+        image: [Bfem,Bfem_1,Bfem_2,Bfem_3],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "WomFashion",
+        name: "Women Fashion Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 100,
+        image: [Homef_24,Homef_24_1,Homef_24_2,HomeKit24_3],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+     {
+        _id: "fashionMen",
+         name: "Men Fashion Jersey",
+         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+         price: 100,
+         image: [Fm_24,Fm_24_1,Fm_24_2,Fm_24_3],
+         category: "Men",
+         subCategory: "Topwear",
+         sizes: ["S", "M", "L"],
+         date: 1716634345448,
+         bestseller: true 
+     },
+     {
+        _id: "TrainingPant",
+         name: "Women Training Pant Pink",
+         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+         price: 100,
+         image: [BottomDP,BottomDP2,BottomDP3],
+         category: "Women",
+         subCategory: "Bottomwear",
+         sizes: ["S", "M", "L"],
+         date: 1716634345448,
+         bestseller: false 
+     },
+     {
+        _id: "TrainingShorts",
+         name: "Women Training Shorts",
+         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+         price: 100,
+         image: [TShortWB,TShortWB1,TShortWB2,TShortWB3],
+         category: "Women",
+         subCategory: "Bottomwear",
+         sizes: ["S", "M", "L"],
+         date: 1716634345448,
+         bestseller: true 
+     },
+     {
+        _id: "TrainingShortsforWomen",
+         name: "Women Training Shorts Black",
+         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+         price: 100,
+         image: [UShortsWB,UShortsWB1,UShortsWB2,UShortsWB3],
+         category: "Women",
+         subCategory: "Bottomwear",
+         sizes: ["S", "M", "L"],
+         date: 1716634345448,
+         bestseller: true 
+     },
+     {
+        _id: "trainingMen",
+         name: "Training Men Kit",
+         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+         price: 100,
+         image: [Tm_24,Tm_24_1,Tm_24_2,Tm_24_3],
+         category: "Men",
+         subCategory: "Topwear",
+         sizes: ["S", "M", "L"],
+         date: 1716634345448,
+         bestseller: true 
+     },
+     {
+        _id: "WomHome",
+        name: "Women Home Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 100,
+        image: [HomeKit24,HomeKit24_1,HomeKit24_2,HomeKit24_3],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+     {
+       _id: "menblack",
+        name: "Men Black Jersey",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 100,
+        image: [men_b_24,men_b_24_1,men_b_24_2,men_b_24_3],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true 
+    },
+    {
+        _id: "TrainingPant",
+         name: "Women Training Pant",
+         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+         price: 100,
+         image: [BottomP,BottomP1],
+         category: "Women",
+         subCategory: "Bottomwear",
+         sizes: ["S", "M", "L"],
+         date: 1716634345448,
+         bestseller: true 
+     },
+    
+
+]
